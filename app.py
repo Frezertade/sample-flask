5c2714ed01c24936a5f249d14b3abe3802942c5c
frzertade <frezertade@gmail.com>
2021-06-19 22:25:29 -0400

    add this file

--- app.py
+++ app.py
@@ -7,19 +7,23 @@ from bson.json_util import dumps
 app = Flask(__name__)
 
 
-myclient = pymongo.MongoClient(
-    "mongodb://fre:frezer@cluster0-shard-00-00.7xkdf.mongodb.net:27017,cluster0-shard-00-01.7xkdf.mongodb.net:27017,cluster0-shard-00-02.7xkdf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-11kjk0-shard-0&authSource=admin&retryWrites=true&w=majority")
-mydb = myclient["mydatabase"]
-mycol = mydb["News"]
-mycolAbiy = mydb["abiyahmadali"]
-mycolTikvah = mydb["tikvah"]
-mycolBisrat = mydb["BisratSport"]
-mycolEbc = mydb["ebc"]
+# myclient = pymongo.MongoClient(
+#     "mongodb://fre:frezer@cluster0-shard-00-00.7xkdf.mongodb.net:27017,cluster0-shard-00-01.7xkdf.mongodb.net:27017,cluster0-shard-00-02.7xkdf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-11kjk0-shard-0&authSource=admin&retryWrites=true&w=majority")
+# mydb = myclient["mydatabase"]
+# mycol = mydb["News"]
+# mycolAbiy = mydb["abiyahmadali"]
+# mycolTikvah = mydb["tikvah"]
+# mycolBisrat = mydb["BisratSport"]
+# mycolEbc = mydb["ebc"]
 
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/api")
+def hello_world():
+    return "Hello Mitu"
 
 @app.route('/news', methods=['GET'])
 def get_all_stars():
