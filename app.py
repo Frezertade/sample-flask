cc689fd4d37c77c3c4b753cae627fae024ea4169
frzertade <frezertade@gmail.com>
2021-06-20 22:57:53 -0400

    add this file

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/news")
+def hello_world():
+    return "Your number one news source"
